a76f0bf65350af1635401ace5166614682067087
francois-minaret <francois.minaret@gmail.com>
2025-07-08 17:38:43 +0200

    s3 modif

--- tests/upload_s3.py
+++ tests/upload_s3.py
@@ -6,14 +6,14 @@ s3_client = boto3.client("s3")
 # Définition des paramètres
 # file_name = "unit-tests.xml"  # Nom du fichier XML généré par pytest
 file_name = "results.xml"
-bucket_name = "s3://s3lead-fmi/MLOPS"
-s3_key = "unit-tests.xml"  # Chemin dans le bucket
+bucket_name = "s3lead-fmi"
+s3_key = "/MLOPS/test-results/unit-tests.xml"  # Chemin dans le bucket
 
 # Upload du fichier
 try:
     s3_client.upload_file(file_name, bucket_name, s3_key)
     print(
-        f"✅ Fichier '{file_name}' envoyé avec succès sur 's3://s3lead-fmi/MLOPS/test-results/{s3_key}'"
+        f"✅ Fichier '{file_name}' envoyé avec succès sur 's3://s3lead-fmi{s3_key}'"
     )
 except Exception as e:
     print(f"❌ Erreur lors de l'upload sur S3 : {e}")
